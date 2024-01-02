dd4bd17bc58df69e3fd636a7a72d89f0f43a4146
chebon254 <kelvinchebon90@gmail.com>
2024-01-02 18:19:41 +0300

    Code Changes

--- axtrion/settings.py
+++ axtrion/settings.py
@@ -118,13 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'main/static']
+STATIC_ROOT = [BASE_DIR / 'main', 'static/']
 
 import os
 
 # Add this at the end of the file
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'main/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'main', 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
